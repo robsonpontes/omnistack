0d382a637770eca8fd837ba7076bc763820687f0
robsonpontes <robson.tecnologia@gmail.com>
2019-04-10 20:59:26 -0300

    Adiciona variavel ambiente

--- src/models/File.js
+++ src/models/File.js
@@ -14,13 +14,15 @@ const File = new mongoose.Schema(
     },
     {
         timestamps: true,
-        toObject: {virtuals: true},
-        toJSON: {virtuals: true}
+        toObject: { virtuals: true },
+        toJSON: { virtuals: true }
     }
 );
 
-File.virtual('url').get(function(){
-    return `http://localhost:3333/files/${encodeURIComponent(this.path)}`
+File.virtual('url').get(function () {
+    const url = process.env.URL || 'http://localhost:3333';
+
+    return `${url}/files/${encodeURIComponent(this.path)}`
 })
 
 module.exports = mongoose.model("File", File);
